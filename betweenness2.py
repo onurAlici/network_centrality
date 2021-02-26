c2ede130b85a5c782d475b0fc4aa8d3d8e314d6e
onurAlici <48276457+onurAlici@users.noreply.github.com>
2021-02-27 02:15:23 +0300

    Update betweenness2.py

--- betweenness2.py
+++ betweenness2.py
@@ -8,23 +8,26 @@ import jenkspy
 
 
 def read_data(fp):
-    "reads the file for network data, data needs to be linestring layer"
+    """reads the file for network data, data needs to be linestring layer"""
     data = gpd.read_file(fp)
     return data
 
 def p2g(data):
-    "transforms the geopandas dataframe to networkx graph, length of the lines calculated with numpy norm function"
+    """transforms the geopandas dataframe to networkx graph, length of the lines calculated with numpy norm function"""
     graph1 = nx.Graph()
 
     for count, i in enumerate(data["geometry"]):
+       #every geometry can be a multilinestring and I converted every multilinestring to separate linestring, to represent them in network 
         first = 0
         filtre = data.iloc[count]["ref"]
+        #filter comes from openstreetmap(OSM) data which has a ref column. I mainly used osm as a source for tranportation networks.
         for j in list(i.coords):
             if first == 0:
                 first += 1
                 n1 = j
             else:
                 n2 = j
+               #every point of linestring used as vertex in graph and linestrings become edges.
                 graph1.add_edge(n1, n2, weight=np.linalg.norm(np.array((n1[0], n1[1])) - np.array((n2[0], n2[1]))),filtre = filtre )
                 
                 n1 = j
@@ -32,7 +35,7 @@ def p2g(data):
     return graph1
 
 def g2p(graph,data):
-    "after calculating the edge betweenness for the lines transforms the networkx graph to geopandas dataframe "
+    """after calculating the edge betweenness for the lines transforms the networkx graph to geopandas dataframe """
     bet = nx.edge_betweenness_centrality(graph,k=750,weight="weight")
     layer = gpd.GeoDataFrame()
     layer.crs = data.crs
@@ -54,13 +57,21 @@ graph = p2g(data)
 
 sonuc = g2p(graph, data)
 
-breaks = jenkspy.jenks_breaks(sonuc["weight"], nb_class=5)
+data2 = sonuc.copy()
+sonuc.to_file("test.gpkg", layer = "orginal", driver="GPKG")
 
-for count, i in enumerate(breaks[::-1][:-1]):
+for i in range(3):
+    """
+    this is iteration part where in every iteration centrality of the network calculated again and edges are classified in five groups according to their 
+    betweenness centrality. After classification of edges, top two classes extracted from network and a next iteration begins. 
+    """
+    print("cycle    " + str(i) + "    in progress")
+    breaks = jenkspy.jenks_breaks(data2["weight"], nb_class=5)
+    breaks.reverse()
     
-    filtered = (sonuc["weight"] > breaks[count]) & (sonuc["weight"] < breaks[count+1])
-    data2 = sonuc[~filtered]
+    filtered = ((data2["weight"] > breaks[2]) & (data2["weight"] < breaks[0]))
+    #filter can be used for granting immunity to some edges in the network, such as bridges or anything else too inconvenient to remove from network
+    data2 = data2[~filtered]
     data2 = g2p(p2g(data2), sonuc)
-    data2.to_file("test" + str(count))
-
-
+    #every iteration saved to file
+    data2.to_file("test.gpkg", layer=str(i), driver="GPKG")
